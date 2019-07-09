1c03d23f0089b592d8f3771df4dd4ffe00b5a1f0
BaYongJian <52686709+BaYongJian@users.noreply.github.com>
2019-07-09 22:27:35 +0800

    Update and rename code.java to code1.java

--- 剑指offer/合并两个排序的链表/code1.java
+++ 剑指offer/合并两个排序的链表/code1.java
@@ -9,9 +9,6 @@ public class ListNode {
 }*/
 public class Solution {
     public ListNode Merge(ListNode list1,ListNode list2) {
-        if(list1 == null && list2 == null) return null;
-        if(list1 == null) return list2;
-        if(list2 == null) return list1;
         ListNode list;
         if(list1.val > list2.val){
             list = list2;
