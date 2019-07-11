9125111d69a39e1194e8b06c3777e2dc25a2ccd8
BaYongJian <52686709+BaYongJian@users.noreply.github.com>
2019-07-11 16:55:21 +0800

    Create code2.java

---
+++ 剑指offer/矩形覆盖/code2.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public int RectCover(int target) {
+        if(target < 1) return 0;
+        if(target == 1) return 1;
+        if(target == 2) return 2;
+        return RectCover(target - 1) + RectCover(target - 2);
+    }
+}
