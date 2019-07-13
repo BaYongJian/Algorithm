052e20471075c2fdef529783d8418b460f4e05f8
BaYongJian <52686709+BaYongJian@users.noreply.github.com>
2019-07-13 15:43:27 +0800

    Update code1.java

--- 剑指offer/二维数组中的查找/code1.java
+++ 剑指offer/二维数组中的查找/code1.java
@@ -1,5 +1,8 @@
 public class Solution {
     public boolean Find(int target, int [][] array) {
+        if(array.length==0||array[0].length==0||target<array[0][0] ||target>array[array.length-1][array[0].length-1]){
+            return false;
+        }
         for(int i = 0;i < array.length;i++){
             for(int j = 0;j < array[i].length;j++){
                 if(array[i][j] == target){
