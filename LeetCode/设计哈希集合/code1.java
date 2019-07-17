7dd8771df1dffcfe8e4d70ee2e20539e659886f0
BaYongJian <52686709+BaYongJian@users.noreply.github.com>
2019-07-17 10:41:15 +0800

    Create code1.java

---
+++ LeetCode/设计哈希集合/code1.java
@@ -0,0 +1,29 @@
+class MyHashSet {
+
+    /** Initialize your data structure here. */
+    int[] nums;
+    public MyHashSet() {
+        nums = new int[1000000];
+    }
+    
+    public void add(int key) {
+        nums[key] = 1;
+    }
+    
+    public void remove(int key) {
+        nums[key] = 0;
+    }
+    
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        return nums[key] == 1;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
