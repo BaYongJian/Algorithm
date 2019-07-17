172695405c007714dae743aa84c014e673f32845
BaYongJian <52686709+BaYongJian@users.noreply.github.com>
2019-07-17 16:01:27 +0800

    Create code2.java

---
+++ 剑指offer/数组中只出现一次的数字/code2.java
@@ -0,0 +1,36 @@
+//num1,num2分别为长度为1的数组。传出参数
+//将num1[0],num2[0]设置为返回结果
+public class Solution {
+    public void FindNumsAppearOnce(int [] array,int num1[] , int num2[]) {
+        if(array.length == 2){
+            num1[0] = array[0];
+            num2[0] = array[1];
+            return;
+        }
+        int bitResult = 0;
+        for(int i = 0; i < array.length; ++i){
+            bitResult ^= array[i];
+        }
+        int index = findFirst1(bitResult);
+        for(int i = 0; i < array.length; ++i){
+            if(isBit1(array[i], index)){
+                num1[0] ^= array[i];
+            }else{
+                num2[0] ^= array[i];
+            }
+        }
+    }
+     
+    private int findFirst1(int bitResult){
+        int index = 0;
+        while(((bitResult & 1) == 0) && index < 32){
+            bitResult >>= 1;
+            index++;
+        }
+        return index;
+    }
+     
+    private boolean isBit1(int target, int index){
+        return ((target >> index) & 1) == 1;
+    }
+}
