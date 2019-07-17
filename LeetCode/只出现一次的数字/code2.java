58ab9c6c69890f72890f415b095e0544241b3277
BaYongJian <52686709+BaYongJian@users.noreply.github.com>
2019-07-17 13:48:15 +0800

    Create code2.java

---
+++ LeetCode/只出现一次的数字/code2.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int val = 0;
+        for(int i = 0;i < nums.length;i++){
+            val = val ^ nums[i];
+        }
+        return val;
+    }
+}
