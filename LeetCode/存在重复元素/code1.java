b5992e1a2948efe2379188c97b64ca81e6c3de7e
BaYongJian <52686709+BaYongJian@users.noreply.github.com>
2019-07-18 13:23:35 +0800

    Create code1.java

---
+++ LeetCode/存在重复元素/code1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int x : nums){
+            if(set.contains(x)){
+                return true;
+            }else{
+                set.add(x);
+            }
+        }
+        return false;
+    }
+}
