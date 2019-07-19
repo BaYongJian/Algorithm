410cd0b1b11a2f8f8f24b644188d0a0d8e8ab0d2
BaYongJian <52686709+BaYongJian@users.noreply.github.com>
2019-07-19 17:01:44 +0800

    Create code2.java

---
+++ 剑指offer/和为S的连续正数序列/code2.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+public class Solution {
+    public ArrayList<ArrayList<Integer> > FindContinuousSequence(int sum) {
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        for (int n = (int) Math.sqrt(2 * sum); n >= 2; n--) {
+            if ((n & 1) == 1 && sum % n == 0 || (sum % n) * 2 == n) {
+                ArrayList<Integer> list = new ArrayList<>();
+                for (int j = 0, k = (sum / n) - (n - 1) / 2; j < n; j++, k++) {
+                    list.add(k);
+                }
+                ans.add(list);
+            }
+        }
+        return ans;
+    }
+}
