73e9a630496b3e44b99dcd94b1ab5acde1b6ff0d
BaYongJian <52686709+BaYongJian@users.noreply.github.com>
2019-07-20 15:51:43 +0800

    Create code2.java

---
+++ 剑指offer/第一个只出现一次的字符/code2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Solution {
+    public int FirstNotRepeatingChar(String s) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i = 0;i < s.length();i++){
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        for(int i = 0;i < s.length();i++){
+            if(map.get(s.charAt(i)) == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
