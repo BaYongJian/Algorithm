71d683116178787085a8b1e206e301c960eca634
BaYongJian <52686709+BaYongJian@users.noreply.github.com>
2019-07-20 15:06:45 +0800

    Update code.java

--- LeetCode/两个数组的交集Ⅱ/code.java
+++ LeetCode/两个数组的交集Ⅱ/code.java
@@ -16,7 +16,7 @@ class Solution {
         for(int i = 0;i < len;i++){
             num[i] = list.poll();
         }
-        //注意，上面不能写成下面这样，因为for循环中，每次都重新判断了list的长度，如果如下面这样写，则美循环一次，i自增一，list长度自减一。
+        //注意，上面不能写成下面这样，因为for循环中，每次都重新判断了list的长度，如果如下面这样写，则每循环一次，i自增一，list长度自减一。
         // int[] num = new int[list.size()];
         // for(int i = 0;i < list.size();i++){
         //     num[i] = list.poll();
