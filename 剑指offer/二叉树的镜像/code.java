56200b3257e1f75d3c82a20b07e6fd8d2c6ea2d4
BaYongJian <52686709+BaYongJian@users.noreply.github.com>
2019-07-23 15:55:20 +0800

    Create code.java

---
+++ 剑指offer/二叉树的镜像/code.java
@@ -0,0 +1,23 @@
+/**
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+public class Solution {
+    public void Mirror(TreeNode root) {
+        if(root == null) return;
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        Mirror(root.left);
+        Mirror(root.right);
+    }
+}
