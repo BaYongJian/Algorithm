e56116e19cd8ee5ac2a875ba029a9a300715546c
BaYongJian <52686709+BaYongJian@users.noreply.github.com>
2019-07-25 14:52:13 +0800

    Update code2.java

--- 剑指offer/调整数组顺序使奇数位于偶数前面/code2.java
+++ 剑指offer/调整数组顺序使奇数位于偶数前面/code2.java
@@ -1,3 +1,4 @@
+//从后往前
 public class Solution {
     public void reOrderArray(int [] array) {
         for (int i = 0;i < array.length;i++) {
@@ -6,6 +7,21 @@ public class Solution {
                     int a = array[j];
                     array[j] = array[j - 1];
                     array[j - 1] = a;
+                }
+            }
+        }
+    }
+}
+
+//从前往后
+public class Solution {
+    public void reOrderArray(int [] array) {
+        for(int i = 0;i < array.length;i++){
+            for(int j = 0;j < array.length - 1;j++){
+                if(array[j]%2 == 0 && array[j + 1]%2 == 1){
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
             }
         }
