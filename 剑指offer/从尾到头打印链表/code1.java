2f4cabfe608ffd68b868c4d23e53fc6af203e276
BaYongJian <52686709+BaYongJian@users.noreply.github.com>
2019-07-25 10:13:05 +0800

    Update code1.java

--- 剑指offer/从尾到头打印链表/code1.java
+++ 剑指offer/从尾到头打印链表/code1.java
@@ -1,6 +1,18 @@
+/**
+*    public class ListNode {
+*        int val;
+*        ListNode next = null;
+*
+*        ListNode(int val) {
+*            this.val = val;
+*        }
+*    }
+*
+*/
+import java.util.ArrayList;
 public class Solution {
+    ArrayList<Integer> list = new ArrayList<Integer>();
     public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
-        ArrayList<Integer> list = new ArrayList<Integer>();
         if(listNode != null){
             printListFromTailToHead(listNode.next);
             list.add(listNode.val);
