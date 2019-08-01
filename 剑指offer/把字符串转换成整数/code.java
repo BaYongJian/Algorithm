7d28f2b82c5ce88bb7e6149e61570d018facfc6d
BaYongJian <52686709+BaYongJian@users.noreply.github.com>
2019-08-01 17:18:01 +0800

    Update code.java

--- 剑指offer/把字符串转换成整数/code.java
+++ 剑指offer/把字符串转换成整数/code.java
@@ -6,10 +6,12 @@ public class Solution {
         if(a[0] == '-') symbol = -1;
         else symbol = 1;
         int sum = 0;
-        for(char x : a){
-            if((x  == '-') || (x == '+')) continue;    //首位是符号
-            if (x < 48 || x > 57) return 0;    //不是阿拉伯数字
-            sum = sum * 10 + x - 48; //数字0的ASCII为48
+        for(int i = 0;i < a.length;i++){
+            if(i == 0){
+                if((a[0]  == '-') || (a[0] == '+')) continue;    //首位是符号
+            }
+            if (a[i] < 48 || a[i] > 57) return 0;    //不是阿拉伯数字
+            sum = sum * 10 + a[i] - 48; //数字0的ASCII为48
         }
         return sum * symbol;
     }
