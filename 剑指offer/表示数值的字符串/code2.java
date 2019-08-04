15cd38d64c7dd61b1c7f47c5d2f6269d841e557a
BaYongJian <52686709+BaYongJian@users.noreply.github.com>
2019-08-04 14:21:48 +0800

    Create code2.java

---
+++ 剑指offer/表示数值的字符串/code2.java
@@ -0,0 +1,6 @@
+public class Solution {
+    public boolean isNumeric(char[] str) {
+        String string = String.valueOf(str);
+        return string.matches("[\\+\\-]?\\d*(\\.\\d+)?([eE][\\+\\-]?\\d+)?");
+    }
+}
