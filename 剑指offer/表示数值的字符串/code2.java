8ef832252d51c76a790b59a6376e7422e726fb70
BaYongJian <52686709+BaYongJian@users.noreply.github.com>
2019-08-06 13:30:00 +0800

    Update code2.java

--- 剑指offer/表示数值的字符串/code2.java
+++ 剑指offer/表示数值的字符串/code2.java
@@ -1,6 +1,6 @@
 public class Solution {
     public boolean isNumeric(char[] str) {
         String string = String.valueOf(str);
-        return string.matches("[\\+\\-]?\\d*(\\.\\d+)?([eE][\\+\\-]?\\d+)?");
+        return string.matches("^[\\+\\-]?\\d*(\\.\\d+)?([eE][\\+\\-]?\\d+)?");
     }
 }
