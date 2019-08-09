934d0d1dc31d51c02721ccd401a3ce1085f23ae0
BaYongJian <52686709+BaYongJian@users.noreply.github.com>
2019-08-09 17:47:54 +0800

    Create code1.java

---
+++ LeetCode/缺失数字/code1.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int num = nums.length;
+        for(int i = 0;i < nums.length;i++){
+            num ^= i ^ nums[i];
+        }
+        return num;
+    }
+}
